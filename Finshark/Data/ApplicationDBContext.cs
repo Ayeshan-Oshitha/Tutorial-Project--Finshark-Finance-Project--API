2c8c369a2ee83cd1b6c5c6e7a49574fd3af52d54
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-09-21 09:09:44 +0530

    22. Register

--- Finshark/Data/ApplicationDBContext.cs
+++ Finshark/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Finshark.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +13,26 @@ namespace Finshark.Data
         }
 
         public DbSet<Stock> Stocks { get; set; }
-        public DbSet<Comment> Comments { get; set; } 
+        public DbSet<Comment> Comments { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
 
     }
 }
